1226480d0870997543df7dac9cc9fdbd74402225
15670339118@qq.com <15670339118@qq.com>
2019-07-15 21:15:21 +0800

    修改订单前段页面内容

---
+++ tb_store/tb_store/apps/orders/serializers.py
@@ -0,0 +1,22 @@
+from rest_framework import serializers
+
+from goods.models import SKU
+
+
+class CartSKUSerializer(serializers.ModelSerializer):
+    """
+    购物车商品数据序列化器
+    """
+    count = serializers.IntegerField(label='数量')
+
+    class Meta:
+        model = SKU
+        fields = ('id', 'name', 'default_image_url', 'price', 'count')
+
+
+class OrderSettlementSerializer(serializers.Serializer):
+    """
+    订单结算数据序列化器
+    """
+    freight = serializers.DecimalField(label='运费', max_digits=10, decimal_places=2)
+    skus = CartSKUSerializer(many=True)
--- tb_store/tb_store/apps/orders/views.py
+++ tb_store/tb_store/apps/orders/views.py
@@ -1,3 +1,44 @@
+from decimal import Decimal
 from django.shortcuts import render
 
 # Create your views here.
+from django_redis import get_redis_connection
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from goods.models import SKU
+from orders.serializers import OrderSettlementSerializer
+
+
+class OrderSettlementView(APIView):
+    """
+    订单结算
+    """
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        """
+        获取
+        """
+        user = request.user
+
+        # 从购物车中获取用户勾选要结算的商品信息
+        redis_conn = get_redis_connection('cart')
+        redis_cart = redis_conn.hgetall('cart_%s' % user.id)
+        cart_selected = redis_conn.smembers('cart_selected_%s' % user.id)
+
+        cart = {}
+        for sku_id in cart_selected:
+            cart[int(sku_id)] = int(redis_cart[sku_id])
+
+        # 查询商品信息
+        skus = SKU.objects.filter(id__in=cart.keys())
+        for sku in skus:
+            sku.count = cart[sku.id]
+
+        # 运费
+        freight = Decimal('10.00')
+
+        serializer = OrderSettlementSerializer({'freight': freight, 'skus': skus})
+        return Response(serializer.data)
