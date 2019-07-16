30cb5451739e9b1705f3dc4bf07fbb493cb4f641
15670339118@qq.com <15670339118@qq.com>
2019-07-16 18:18:01 +0800

    修复提交订单的时候，商品接口BUG

--- tb_store/tb_store/urls.py
+++ tb_store/tb_store/urls.py
@@ -24,5 +24,6 @@ urlpatterns = [
     url(r"", include("areas.urls")),
     url(r'^ckeditor/', include('ckeditor_uploader.urls')),
     url(r'', include("goods.urls")),
-    url(r'', include("carts.urls"))
+    url(r'', include("carts.urls")),
+    url(r'', include("orders.urls")),
 ]
