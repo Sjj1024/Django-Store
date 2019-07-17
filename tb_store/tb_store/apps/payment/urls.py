dcd05f2cc91f4ea451dff6999c7f8740d7a33307
15670339118@qq.com <15670339118@qq.com>
2019-07-17 18:38:32 +0800

    创建支付订单数据模型类

---
+++ tb_store/tb_store/apps/payment/urls.py
@@ -0,0 +1,7 @@
+from django.conf.urls import url
+
+from . import views
+
+urlpatterns = [
+    url(r'^orders/(?P<order_id>\d+)/payment/$', views.Payment.as_view()),
+]
--- tb_store/tb_store/apps/payment/views.py
+++ tb_store/tb_store/apps/payment/views.py
@@ -1,19 +1,46 @@
-from django.db import models
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-from tb_store.utils.models import BaseModel
 from orders.models import OrderInfo
 
-# Create your models here.
 
-
-class Payment(BaseModel):
+class PaymentView(APIView):
     """
-    支付信息
+    支付
     """
-    order = models.ForeignKey(OrderInfo, on_delete=models.CASCADE, verbose_name='订单')
-    trade_id = models.CharField(max_length=100, unique=True, null=True, blank=True, verbose_name="支付编号")
+    permission_classes = (IsAuthenticated,)
 
-    class Meta:
-        db_table = 'tb_payment'
-        verbose_name = '支付信息'
-        verbose_name_plural = verbose_name
+    def get(self, request, order_id):
+        """
+        获取支付链接
+        """
+        # 判断订单信息是否正确
+        try:
+            order = OrderInfo.objects.get(order_id=order_id, user=request.user,
+                                          pay_method=OrderInfo.PAY_METHODS_ENUM["ALIPAY"],
+                                          status=OrderInfo.ORDER_STATUS_ENUM["UNPAID"])
+        except OrderInfo.DoesNotExist:
+            return Response({'message': '订单信息有误'}, status=status.HTTP_400_BAD_REQUEST)
+
+        # 构造支付宝支付链接地址
+        alipay = AliPay(
+            appid=settings.ALIPAY_APPID,
+            app_notify_url=None,  # 默认回调url
+            app_private_key_path=os.path.join(os.path.dirname(os.path.abspath(__file__)), "keys/app_private_key.pem"),
+            alipay_public_key_path=os.path.join(os.path.dirname(os.path.abspath(__file__)), "keys/alipay_public_key.pem"),  # 支付宝的公钥，验证支付宝回传消息使用，不是你自己的公钥,
+            sign_type="RSA2",  # RSA 或者 RSA2
+            debug=settings.ALIPAY_DEBUG  # 默认False
+        )
+
+        order_string = alipay.api_alipay_trade_page_pay(
+            out_trade_no=order_id,
+            total_amount=str(order.total_amount),
+            subject="美多商城%s" % order_id,
+            return_url="http://www.meiduo.site:8080/pay_success.html",
+        )
+        # 需要跳转到https://openapi.alipay.com/gateway.do? + order_string
+        # 拼接链接返回前端
+        alipay_url = settings.ALIPAY_URL + "?" + order_string
+        return Response({'alipay_url': alipay_url})
