cbff361fd5ba48b98845bff489ad14a1aef019f6
15670339118@qq.com <15670339118@qq.com>
2019-07-18 19:19:56 +0800

    创建支付成功后的回调接口

--- tb_store/tb_store/apps/payment/urls.py
+++ tb_store/tb_store/apps/payment/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     url(r'^orders/(?P<order_id>\d+)/payment/$', views.PaymentView.as_view()),
+    url(r'^payment/status/$', views.PaymentStatusView.as_view())
 ]
--- tb_store/tb_store/apps/payment/views.py
+++ tb_store/tb_store/apps/payment/views.py
@@ -6,6 +6,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from orders.models import OrderInfo
+from payment.models import Payment
 from tb_store.settings import dev
 
 
@@ -48,3 +49,40 @@ class PaymentView(APIView):
         # 拼接链接返回前端
         alipay_url = dev.ALIPAY_URL + "?" + order_string
         return Response({'alipay_url': alipay_url})
+
+
+# 支付宝回调函数接口 put /payment/status/?支付宝参数
+class PaymentStatusView(APIView):
+    def put(self, request):
+        # 接收参数，效验参数
+        # 构造支付宝支付链接地址
+        alipay_req_data = request.query_params
+        if not alipay_req_data:
+            return Response({"message": "缺啥参数"}, status=status.HTTP_400_BAD_REQUEST)
+        alipay_req_dict = alipay_req_data.dict()
+        sign = alipay_req_dict.pop("sign")
+
+        alipay = AliPay(
+            appid=dev.ALIPAY_APPID,
+            app_notify_url=None,  # 默认回调url
+            app_private_key_path=os.path.join(os.path.dirname(os.path.abspath(__file__)), "keys/app_private_key.pem"),
+            alipay_public_key_path=os.path.join(os.path.dirname(os.path.abspath(__file__)),
+                                                "keys/alipay_public_key.pem"),  # 支付宝的公钥，验证支付宝回传消息使用，不是你自己的公钥,
+            sign_type="RSA2",  # RSA 或者 RSA2
+            debug=dev.ALIPAY_DEBUG  # 默认False
+        )
+
+        result = alipay.verify(alipay_req_dict, sign)
+        # 保存数据，保存支付结果数据
+        if result:
+            order_id = alipay_req_dict.get("out_trade_no")
+            trade_id = alipay_req_dict.get("trade_no")
+            # 修改订单状态
+            Payment.objects.create(
+                order_id=order_id,
+                trade_id=trade_id
+            )
+            OrderInfo.objects.filter(order_id=order_id).update(status=OrderInfo.ORDER_STATUS_ENUM["UNCOMMENT"])
+            return Response({"trade_id": trade_id})
+        else:
+            return Response({"message": "参数有误"}, status=status.HTTP_400_BAD_REQUEST)
